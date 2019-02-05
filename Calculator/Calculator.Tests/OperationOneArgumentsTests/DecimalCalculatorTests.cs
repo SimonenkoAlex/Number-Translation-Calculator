a5ace87a16ca3c0621ddc568d3c548ffa8862c51
SimonenkoAlex <alexsim99.99@mail.ru>
2019-02-05 16:43:27 +0600

    Another unit test

---
+++ Calculator/Calculator.Tests/OperationOneArgumentsTests/DecimalCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Calculator.OperationOneArguments;
+
+namespace Calculator.Tests.OperationOneArgumentsTests
+{
+    [TestFixture]
+    class DecimalCalculatorTests
+    {
+        [Test]
+        public void DecimalCalculatorTest()
+        {
+            DecimalCalculator calculator = new DecimalCalculator();
+            Assert.AreEqual(7, calculator.Calculate("111"));
+        }
+    }
+}
