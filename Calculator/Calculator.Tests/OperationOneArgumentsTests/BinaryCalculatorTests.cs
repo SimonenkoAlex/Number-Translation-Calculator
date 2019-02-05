96263e161f3048c49df9ed320cc5568d31475906
SimonenkoAlex <alexsim99.99@mail.ru>
2019-02-05 16:36:00 +0600

    Unit tests, advanced testing and testing factories

---
+++ Calculator/Calculator.Tests/OperationOneArgumentsTests/BinaryCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Calculator.OperationOneArguments;
+
+namespace Calculator.Tests.OperationOneArgumentsTests
+{
+    [TestFixture]
+    class BinaryCalculatorTests
+    {
+        [Test]
+        public void BinaryCalculatorTest()
+        {
+            BinaryCalculator calculator = new BinaryCalculator();
+            Assert.AreEqual(111, calculator.Calculate("7"));
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/OperationOneArgumentsTests/OneArgumentsFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Calculator.OperationOneArguments;
+
+namespace Calculator.Tests.OperationOneArgumentsTests
+{
+    class OneArgumentsFactoryTests
+    {
+        [TestCase("btnBinary", typeof(BinaryCalculator))]
+        [TestCase("btnDecimal", typeof(DecimalCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
